beb8de487a408a9510d95f39cad467386e86c70f
akella <akella.a@gmail.com>
2014-09-25 11:46:18 +0300

    videos

--- js/common.js
+++ js/common.js
@@ -1,8 +1,32 @@
-head.ready(function() {
+$(document).ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	$('body').addClass('a');
 
-	console.log($('body').html());
+	// function supports_video() {
+ //  		return !!document.createElement('video').canPlayType;
+	// }
+	// var video = "";
+	// video += '<video id="video"   muted loop autoplay>';
+	// video += 	'<source src="video/video.ogv" type="video/ogg">';
+	// video += 	'<source src="video/video.mp4" type="video/mp4">';
+	// video += 	'<source src="video/video.webm" type="video/webm">';
+	// video +=  '</video>';
+
+
+	// if(supports_video()){
+	// 	$('.video').append(video);
+	// 	$('#video').get(0).play();
+	// }
+	$('input:submit').click(function(event) {
+		ff = $(this).parents('form')
+		error = true;
+		$('.input').addClass('is-error');
+		// validation here
+		if(!ff.hasClass('shake') || error){
+			ff.addClass('shake');	
+			setTimeout(function(){ff.removeClass("shake");}, 500);
+		}
+
+		return false;
+	});
 });
