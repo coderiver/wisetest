f2de610b780f1cd73b837f1f582cf629b8413699
akella <akella.a@gmail.com>
2014-09-25 10:23:49 +0300

    fixme

--- js/effects.js
+++ js/effects.js
@@ -1,5 +1,16 @@
 
 $(document).ready(function() {
 	$('body').addClass('a');
+	$('input:submit').click(function(event) {
+		ff = $(this).parents('form')
+		error = true;
+		// validation here
+		if(!ff.hasClass('shake') || error){
+			ff.addClass('shake');	
+			setTimeout(function(){ff.removeClass("shake");}, 500);
+		}
+
+		return false;
+	});
 });
 
